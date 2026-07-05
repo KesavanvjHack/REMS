21c8657a25cb861bb378664a10fe1e469d836f00
KesavanvjHack <g.kesavaperumalvnr@gmail.com>
2026-07-05 21:09:08 +0530

    Auto-create superuser on build

--- backend/build.sh
+++ backend/build.sh
@@ -5,3 +5,18 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create superuser if not exists
+python -c "
+import django
+django.setup()
+from core.models import User
+import os
+email = os.environ.get('SUPERUSER_EMAIL', 'admin@rems.com')
+password = os.environ.get('SUPERUSER_PASSWORD', 'AdminPassword123')
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password, first_name='Admin', last_name='User')
+    print('Superuser created successfully!')
+else:
+    print('Superuser already exists.')
+"
